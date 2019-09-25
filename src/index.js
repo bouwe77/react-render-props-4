05f3878b51fac483243bac3ac8c2e7f5509e5d6e
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-09-25 22:01:18 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  const [subscribe, setSubscribe] = useState(false);
+
+  function changeSubscription(on) {
+    setSubscribe(on);
+  }
+
+  function save(event) {
+    event.preventDefault();
+    console.log(`Let's save subscribe: ${subscribe}`);
+  }
+
+  function renderCheckbox(toggle) {
+    return (
+      <>
+        <input type="checkbox" onChange={toggle} />
+        Subscribe to newsletter
+      </>
+    );
+  }
+
+  return (
+    <form onSubmit={save}>
+      <Toggle render={renderCheckbox} onToggle={changeSubscription} />
+      <br />
+      <button type="submit">Save</button>
+    </form>
+  );
+}
+
+function Toggle({ render, onToggle }) {
+  const [on, setOn] = useState(false);
+
+  function doToggle() {
+    setOn(on => !on);
+    onToggle(!on);
+  }
+
+  return <>{render(doToggle)}</>;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
